9fd9a9c564d346ead7d20da8c26efa3248cd1727
RybsonBybson <miwlod08+1@gmail.com>
2026-02-23 17:56:21 +0100

    ...

--- client.py
+++ client.py
@@ -1,18 +1,20 @@
 import asyncio
 import websockets
 from settings import *
+import easygui
 
 async def chat():
-    uri = f"ws://192.168.1.7:{PORT}"  # <-- tu IP serwera
+    uri = f"ws://192.168.1.7:{PORT}"  # IP serwera
 
     async with websockets.connect(uri) as websocket:
         print("Połączono z serwerem")
 
-        while True:
-            msg = input("Ty: ")
-            await websocket.send(msg)
-
-            reply = await websocket.recv()
-            print("Odpowiedź:", reply)
+        for i in range(5):
+            file_path = easygui.fileopenbox()
+            if file_path:
+                await websocket.send(file_path)
+                await websocket.recv()  # czekamy na odpowiedź
+        
+        # tutaj close nie jest konieczne, 'async with' zrobi to automatycznie
 
 asyncio.run(chat())
