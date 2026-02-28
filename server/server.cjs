3c68c9d4098173a2105a59fc975d2c3055df4a03
RybsonBybson <miwlod08+1@gmail.com>
2026-02-28 22:33:20 +0100

    ..

--- server/server.cjs
+++ server/server.cjs
@@ -1,18 +1,28 @@
+const http = require("http");
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8765 });
+const port = process.env.PORT || 8765;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Serwer działa");
+});
+
+const wss = new WebSocket.Server({ server });
 
 wss.on("connection", ws => {
   console.log("Nowy klient połączony");
 
-  // Odbieranie wiadomości od klienta
   ws.on("message", message => {
-    console.log("Otrzymano:", message);
-    ws.send(`Echo: ${message}`); // odsyłamy wiadomość z powrotem
+    console.log("Otrzymano:", message.toString());
+    ws.send(`Echo: ${message}`);
   });
 
-  // Zamknięcie połączenia
   ws.on("close", () => {
     console.log("Klient rozłączony");
   });
+});
+
+server.listen(port, () => {
+  console.log("Serwer działa na porcie", port);
 });
